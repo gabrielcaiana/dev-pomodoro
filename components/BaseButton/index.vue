639b037872f07a1204c13be361b9326d403a63cd
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-26 23:06:06 -0300

    feat: create button component

---
+++ components/BaseButton/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    class="base-button font-general-bold"
+    :class="{ small: smallButton }"
+    v-on="$listeners"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+
+  props: {
+    small: {
+      type: String,
+      default: null,
+    },
+  },
+
+  computed: {
+    smallButton(): boolean {
+      return !!this.small
+    },
+  },
+})
+</script>
+
+<style scoped>
+.base-button {
+  background: var(--gradient);
+  border: 1px solid #ffb1ee;
+  @apply w-full text-white text-2xl px-1 py-8 hover:opacity-90 transition-all duration-300 rounded-md flex items-center justify-center;
+}
+
+.small {
+  @apply h-8;
+}
+</style>
