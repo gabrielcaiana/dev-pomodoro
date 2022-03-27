cf4fdfad1cf09d20cea357de00df368c78ce18a5
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-27 15:42:01 -0300

    chore: use User module in AppAvatar

--- components/shared/AppAvatar/index.vue
+++ components/shared/AppAvatar/index.vue
@@ -1,28 +1,36 @@
 <template>
-  <div class="app-avatar relative rounded-full">
-    <img
-      src="https://avatars.githubusercontent.com/u/26663338?v=4"
-      alt="Gabriel Caiana"
-      class="w-24 rounded-full p-1"
-    />
+  <div class="flex items-center justify-center gap-6">
+    <div class="font-general-variable">
+      <p class="text-primary-light m-0" v-text="currentUser.name"></p>
+      <p class="m-0" v-text="currentUser.office"></p>
+    </div>
+    <div class="app-avatar relative rounded-full">
+      <img
+        :src="currentUser.avatar"
+        :alt="currentUser.name"
+        class="w-24 rounded-full p-1"
+      />
 
-    <div
-      class="app-avatar-badge rounded-full absolute flex justify-center items-center font-general-bold"
-    >
-      {{ userLevel }}
+      <div
+        class="app-avatar-badge rounded-full absolute flex justify-center items-center font-general-bold"
+      >
+        {{ userLevel }}
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { mapGetters } from 'vuex'
 import Vue from 'vue'
 export default Vue.extend({
   name: 'AppAvatar',
 
   computed: {
-    userLevel(): number {
-      return this.$store.getters['Challenges/currentLevel']
-    },
+    ...mapGetters({
+      userLevel: 'Challenges/currentLevel',
+      currentUser: 'User/currentUser',
+    }),
   },
 })
 </script>
