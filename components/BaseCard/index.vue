6605941e9aeded0320d1ec1fe4a5e9245c8a0410
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-26 23:24:54 -0300

    chore: create BaseCard

---
+++ components/BaseCard/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="base-card w-full rounded-md p-8">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+
+<style scoped>
+.base-card {
+  background: var(--gradient-card);
+  border: 1px solid #ffffff65;
+}
+</style>
