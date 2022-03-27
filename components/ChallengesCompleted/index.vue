d4e6aa6b11d8e47eae8269f873f93542d2615d33
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-27 16:39:21 -0300

    chore: create challengesCompleted component

---
+++ components/ChallengesCompleted/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <BaseCard>
+    <h2 class="font-general-regular text-white">
+      conclua os ciclos para receber novos desafios
+    </h2>
+    <div class="flex justify-between text-white font-general-light-italic mt-4">
+      <span>Concluídos</span>
+      <span v-text="completetedChallenge"></span>
+    </div>
+  </BaseCard>
+</template>
+
+<script lang="ts">
+import { mapGetters } from 'vuex'
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'ChallengesCompleted',
+
+  computed: {
+    ...mapGetters({
+      completetedChallenge: 'Challenges/getCompletedChallenges',
+    }),
+  },
+})
+</script>
