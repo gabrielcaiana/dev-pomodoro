3fdac025000af97b4c7e99a5f7eec447194777ce
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-27 00:06:18 -0300

    chore: create baseIcon

---
+++ components/BaseIcon/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <i :height="size" :width="size" :data-feather="name" :stroke="color"></i>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+
+  props: {
+    name: {
+      type: String,
+      default: null,
+    },
+
+    color: {
+      type: String,
+      default: 'white',
+    },
+
+    size: {
+      type: String,
+      default: '24',
+    },
+  },
+})
+</script>
