1cee2d7568aaef140b6f36166a5ffe5c0eed620c
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-27 18:26:40 -0300

    chore: create leyout default

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-background">
+    <div class="container m-auto p-14">
+      <AppHeader />
+      <Nuxt />
+      <AppFooter />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'LayoutDefault',
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,18 @@
 <template>
-  <div class="bg-background h-screen">
-    <div class="container m-auto p-14">
-      <AppHeader />
+  <main>
+    <div class="flex gap-10 mt-10">
+      <div class="w-full flex">
+        <BaseCard></BaseCard>
+      </div>
 
-      <main>
-        <div class="flex gap-10 mt-10">
-          <div class="w-full flex">
-            <BaseCard></BaseCard>
-          </div>
-
-          <div class="w-full flex flex-col gap-10">
-            <BaseCard>
-              <ProgressBar />
-            </BaseCard>
-            <ChallengesCompleted />
-          </div>
-        </div>
-      </main>
-
-      <AppFooter />
+      <div class="w-full flex flex-col gap-10">
+        <BaseCard>
+          <ProgressBar />
+        </BaseCard>
+        <ChallengesCompleted />
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
