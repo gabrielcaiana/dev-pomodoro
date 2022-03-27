ab16509a8a29c212083631dc5f3221ba8f3810a0
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-27 13:09:47 -0300

    chore: config firestore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,24 +29,25 @@ export default {
   modules: [
     '@nuxtjs/pwa',
     ['cookie-universal-nuxt', { alias: 'cookies' }],
-    '@nuxtjs/firebase',
-  ],
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+          measurementId: process.env.MEASUREMENT_ID,
+        },
 
-  firebase: {
-    config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.MEASUREMENT_ID,
-    },
-    services: {
-      firestore: true,
-      analytics: true,
-    },
-  },
+        services: {
+          firestore: true,
+        },
+      },
+    ],
+  ],
 
   pwa: {
     manifest: {
