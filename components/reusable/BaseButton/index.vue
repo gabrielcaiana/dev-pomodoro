02a38f6218c6ee74f443f2bfa82c7491eafb1bee
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-27 14:15:44 -0300

    refactor: update name components

--- components/reusable/BaseButton/index.vue
+++ components/reusable/BaseButton/index.vue
@@ -12,7 +12,7 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+  name: 'BaseButton',
 
   props: {
     small: {
--- components/reusable/BaseCard/index.vue
+++ components/reusable/BaseCard/index.vue
@@ -8,7 +8,7 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+  name: 'BaseCard',
 })
 </script>
 
