2840cb876a64831f519db56378b3c1ac4354adc2
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-27 02:10:38 -0300

    chore: create countdown module

---
+++ store/Countdown/types.ts
@@ -0,0 +1,28 @@
+import { MutationTree } from 'vuex'
+
+export interface State {
+  time: number
+  isActive: boolean
+  hasCompleted: boolean
+}
+
+export interface Getters {
+  minutes: (state: State) => number
+  seconds: (state: State) => number
+}
+
+export enum Mutations {
+  SET_TIME = 'SET_TIME',
+  RESET_TIME = 'RESET_TIME',
+  SET_IS_ACTIVE = 'SET_IS_ACTIVE',
+  SET_HAS_COMPLETED = 'SET_HAS_COMPLETED',
+}
+
+export type RootState = ReturnType<() => State>
+
+export interface MutationsInterface extends MutationTree<RootState> {
+  [Mutations.SET_TIME](state: State, parameter: number): void
+  [Mutations.RESET_TIME](state: State): void
+  [Mutations.SET_IS_ACTIVE](state: State, parameter: boolean): void
+  [Mutations.SET_HAS_COMPLETED](state: State, parameter: boolean): void
+}
