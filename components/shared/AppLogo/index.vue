ab679531a50571e0d2410bbd0bfdffd5c1590883
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-28 11:39:33 -0300

    chore: create AppLogo component

---
+++ components/shared/AppLogo/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-logo">
+    <h1 class="app-logo-title font-general-bold"><span>Dev</span> Pomodoro</h1>
+    <p class="font-general-italic text-xl">
+      Gerencie seu tempo e seja mais produtivo <br />
+      utilizando o dev pomodoro
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AppLogo',
+})
+</script>
+
+<style scoped>
+.app-logo-title span {
+  background: var(--gradient-text);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
