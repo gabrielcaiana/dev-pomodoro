4e90c263836a41e1b6319fad4a6316d0f38499a4
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-03-28 11:40:12 -0300

    chore: create AppSubtitle component

---
+++ components/reusable/AppSubtitle/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <span class="font-general-medium text-white text-xl"><slot /></span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AppSubtitle',
+})
+</script>
