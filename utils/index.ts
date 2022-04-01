704f59ef1febf6822e7194507640fe96a7c3bed6
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-04-01 01:09:36 -0300

    chore: create splitValue function

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,7 @@
+export const splitValue = (value: string | number, padSize: number = 2) =>
+  `${value}`.padStart(padSize, '0').split('')
+
 export const playAudio = (path: string) => new Audio(path).play()
+
 export const sendNotification = (title: string, options: object) =>
   new Notification(title, options)
